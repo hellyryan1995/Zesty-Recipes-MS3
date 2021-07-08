663f3b6efb8a6e87d7f1c64ba3be43376cf182bc
hellyryan1995 <helly.ryan1995@gmail.com>
2021-07-08 15:30:11 +0000

    Add users profile

--- app.py
+++ app.py
@@ -47,13 +47,15 @@ def signup():
         # add user to "session" cookie
         session["user"] = request.form.get("username").lower()
         flash("Sign Up Successful!")
+        return redirect(url_for("profile", username=session["user"]))
+
     return render_template("signup.html")
 
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
-        #check if username exists in db
+        # check if username exists in db
         existing_user = mongo.db.users.find_one(
             {"username": request.form.get("username").lower()})
 
@@ -61,16 +63,28 @@ def login():
             if check_password_hash(
                 existing_user["password"], request.form.get("password")):
                 session["user"] = request.form.get("username").lower()
-                flash("Welcome to Zesty, {}". format(request.form.get("username")))
+                flash("Welcome to Zesty, {}".format(
+                    request.form.get("username")))
+                return redirect(url_for(
+                    "profile", username=session["user"]))
             else:
-                flash("The Username and/or Password you have entered is Incorrect")
+                flash("The Username and/or Password is Incorrect")
                 return redirect(url_for("login"))
 
-        else: #username doesn't exist
+        else: 
+            # username doesn't exist
             flash("Incorrect Username and/or Password")
             return redirect(url_for("login"))
 
     return render_template("login.html")
+
+
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    # grab username from db
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+    return render_template("profile.html", username=username)
 
 
 if __name__ == "__main__":
