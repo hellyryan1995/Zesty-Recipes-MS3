508a17c0dbe77f762d01b582127fd70afc062096
hellyryan1995 <helly.ryan1995@gmail.com>
2021-07-08 12:07:42 +0000

    Add working login page

--- app.py
+++ app.py
@@ -25,6 +25,7 @@ def view_recipes():
     return render_template("recipes.html", recipes=recipes)
 
 
+# Sign Up Form
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
     if request.method == "POST":
@@ -47,6 +48,29 @@ def signup():
         session["user"] = request.form.get("username").lower()
         flash("Sign Up Successful!")
     return render_template("signup.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        #check if username exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                session["user"] = request.form.get("username").lower()
+                flash("Welcome to Zesty, {}". format(request.form.get("username")))
+            else:
+                flash("The Username and/or Password you have entered is Incorrect")
+                return redirect(url_for("login"))
+
+        else: #username doesn't exist
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
+
+    return render_template("login.html")
 
 
 if __name__ == "__main__":
